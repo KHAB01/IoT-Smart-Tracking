13d85831292f6eb02409be23671aff8b1ab85e66
MoncefKa <moncef.karmaoui@e-polytechnique.ma>
2023-01-06 19:36:49 +0100

    image

---
+++ WebApp/frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Ai from "./Images/ai-lab.png"
+function App() {
+  return (
+    <div className="h-screen w-screen" style={{backgroundImage:`url(${Ai})`,backgroundSize:"contain",backgroundRepeat:"no-repeat",backgroundPosition:"center"}}>
+      <div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
