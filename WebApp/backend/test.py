defef004beb89a44f1421c136855c4b36d37d81a
HananeJB <jabouhanane@gmail.com>
2023-01-28 16:31:36 +0100

    somethinggg

--- WebApp/backend/test.py
+++ WebApp/backend/test.py
@@ -1,37 +1,50 @@
 # Import flask and datetime module for showing date and time
-from flask import Flask
+from flask import Flask, jsonify
 from math import *
 
 
 # Initializing flask app
 app = Flask(__name__)
-captor_1={'x1':350 - 100, 'y1':10}
-captor_2={'x2':0, 'y2':0}
-captor_3={'x3':0, 'y3':0}
-captor_4={'x4':0, 'y4':0}
-captor_5={'x5':0, 'y5':0}
-captor_6={'x6':0, 'y6':0}
+captor_1 = {'x': 412, 'y': 10, 'd': 0}
+captor_2 = {'x': 332, 'y': 80, 'd': 200}
+captor_3 = {'x': 302, 'y': 192.2624969482422, 'd': 0}
+captor_4 = {'x': 332, 'y': 300, 'd': 0}
+captor_5 = {'x': 412, 'y': 375, 'd': 0}
+captor_6 = {'x': 572, 'y': 379.5375061035156, 'd': 0}
+captor_7 = {'x': 677, 'y': 284.5375061035156, 'd': 0}
+captor_8 = {'x': 687, 'y': 120, 'd': 0}
+captor_9 = {'x': 592, 'y': 10, 'd': 0}
 
-
-
+# data = [captor_1, captor_2, captor_3, captor_4, captor_5, captor_6,captor_7,captor_8,captor_9]
+data=[60]
 
 # Route for seeing a data
 @app.route('/data')
 def get_time():
-    # d=150
-    # y= d/(sqrt(1+tan(7.5)**2))
-    # x=sqrt(d**2-y**2)
-	x=10
-	y=2
+    xf = 0
+    yf = 0
+    dataa=[]
+    for i in range(0, len(data)):
+        # if data[i]['d'] != 0 :
+        #     xf = data[i]['x']
+        #     yf = data[i]['y']
+        #     d = data[i]['d']
+        d=data[i]
+        
 
-	# Returning an api for showing in reactjs
-	return {
-		'x':captor_1['x1']-x,
-		"y":captor_1['y1']-y
+        d = d*0.6
+        y = d/(sqrt(1+tan(15)**2))
+        x = sqrt(d**2-y**2)
+        dataa.append({'x': x+captor_1['x'], 'y': y+captor_1['y']})
+        
 
-		}
 
-	
+    
+    return jsonify(dataa)
+    # Returning an api for showing in reactjs
+     
+
+
 # Running app
 if __name__ == '__main__':
-	app.run(host='127.0.0.1/', port=5000)
+    app.run(host='127.0.0.1/', port=5000)
--- WebApp/frontend/src/Map.js
+++ WebApp/frontend/src/Map.js
@@ -19,29 +19,23 @@ const Map = () => {
   const [point9State, setPoint9State] = useState("default");
   const [point10State, setPoint10State] = useState("default");
   
-  const [data, setdata] = useState({
-    x: 0,
-    y:0
-
-});
+  const [data, setData] = useState([]);
 
 // Using useEffect for single rendering
 useEffect(() => {
     // Using fetch to fetch the api from 
     // flask server it will be redirected to proxy
-    fetch("/data").then((res) =>
-        res.json().then((data) => {
-            // Setting a data from api
-            setdata({
-                x: data.x,
-                y: data.y,
-                
-            });
+    const intervalId = setInterval(() => {
+      fetch("/data")
+        .then(res => res.json())
+        .then(data => {
+          setData(data);
 
-            const range = 300;
+         
+         const range = 300;
             const point1 = document.querySelector(".capteur-1");
             const point1Position = point1.getBoundingClientRect();
-            if (data.x > (point1Position.left - 80) && data.x < (point1Position.right + 100) && data.y > (point1Position.top - range) && data.y < (point1Position.bottom + range)) {
+            if (d.x > (point1Position.left - 80) && d.x < (point1Position.right + 100) && d.y > (point1Position.top - range) && d.y < (point1Position.bottom + range)) {
               setPoint1State("highlighted");
             } else {
               setPoint1State("default");
@@ -49,7 +43,7 @@ useEffect(() => {
         
             const point2 = document.querySelector(".capteur-2");
             const point2Position = point2.getBoundingClientRect();
-            if (data.x > (point2Position.left +10)  && data.y > (point2Position.top - 70) && data.y < (point2Position.bottom + 70)) {
+            if (d.x > (point2Position.left +10)  && d.y > (point2Position.top - 70) && d.y < (point2Position.bottom + 70)) {
               setPoint2State("highlighted");
             } else {
               setPoint2State("default");
@@ -57,7 +51,7 @@ useEffect(() => {
         
             const point3 = document.querySelector(".capteur-3");
             const point3Position = point3.getBoundingClientRect();
-            if (data.x > (point3Position.left - 120) && data.x < (point3Position.right + 5) && data.y > (point3Position.top -10) && data.y < (point3Position.bottom +range )) {
+            if (d.x > (point3Position.left - 120) && d.x < (point3Position.right + 5) && d.y > (point3Position.top -10) && d.y < (point3Position.bottom +range )) {
               setPoint3State("highlighted");
             } else {
               setPoint3State("default");
@@ -65,7 +59,7 @@ useEffect(() => {
         
             const point4 = document.querySelector(".capteur-4");
             const point4Position = point4.getBoundingClientRect();
-            if (data.x > (point4Position.left - 140) && data.x < (point4Position.right + 0) && data.y > (point4Position.top - 300) && data.y < (point4Position.bottom + range)) {
+            if (d.x > (point4Position.left - 140) && d.x < (point4Position.right + 0) && d.y > (point4Position.top - 300) && d.y < (point4Position.bottom + range)) {
               setPoint4State("highlighted");
             } else {
               setPoint4State("default");
@@ -73,7 +67,7 @@ useEffect(() => {
         
             const point5 = document.querySelector(".capteur-5");
             const point5Position = point5.getBoundingClientRect();
-            if (data.x > (point5Position.left - 80) && data.x < (point5Position.right + 100) && data.y > (point5Position.top - range) && data.y < (point5Position.bottom + range)) {
+            if (d.x > (point5Position.left - 80) && d.x < (point5Position.right + 100) && d.y > (point5Position.top - range) && d.y < (point5Position.bottom + range)) {
               setPoint5State("highlighted");
             } else {
               setPoint5State("default");
@@ -81,7 +75,7 @@ useEffect(() => {
         
             const point6 = document.querySelector(".capteur-6");
             const point6Position = point6.getBoundingClientRect();
-            if (data.x > (point6Position.left)  && data.x < (point6Position.right+  350) && data.y > (point6Position.top - 70) && data.y < (point6Position.bottom + 70)) {
+            if (d.x > (point6Position.left)  && d.x < (point6Position.right+  350) && d.y > (point6Position.top - 70) && d.y < (point6Position.bottom + 70)) {
               setPoint6State("highlighted");
             } else {
               setPoint6State("default");
@@ -89,7 +83,7 @@ useEffect(() => {
         
             const point7 = document.querySelector(".capteur-7");
             const point7Position = point7.getBoundingClientRect();
-            if (data.x > (point7Position.left - 210) && data.x < (point7Position.right +160) && data.y > (point7Position.top - 100) && data.y < (point7Position.bottom + 10)) {
+            if (d.x > (point7Position.left - 210) && d.x < (point7Position.right +160) && d.y > (point7Position.top - 100) && d.y < (point7Position.bottom + 10)) {
               setPoint7State("highlighted");
             } else {
               setPoint7State("default");
@@ -97,7 +91,7 @@ useEffect(() => {
         
             const point8 = document.querySelector(".capteur-8");
             const point8Position = point8.getBoundingClientRect();
-            if (data.x > (point8Position.left - 270) && data.x < (point8Position.right + 65) && data.y > (point8Position.top - 80) && data.y < (point8Position.bottom + 100)) {
+            if (d.x > (point8Position.left - 150) && d.x < (point8Position.right + 65) && d.y > (point8Position.top - 80) && d.y < (point8Position.bottom + 100)) {
               setPoint8State("highlighted");
             } else {
               setPoint8State("default");
@@ -105,7 +99,7 @@ useEffect(() => {
         
             const point9 = document.querySelector(".capteur-9");
             const point9Position = point9.getBoundingClientRect();
-            if (data.x > (point9Position.left+60) && data.x < (point9Position.right +200) && data.y < (point9Position.top +900) && data.y < (point9Position.bottom + 80)) {
+            if (d.x > (point9Position.left+60) && d.x < (point9Position.right +150) && d.y < (point9Position.top +100) && d.y < (point9Position.bottom + 80)) {
               setPoint9State("highlighted");
             } else {
               setPoint9State("default");
@@ -113,17 +107,21 @@ useEffect(() => {
         
             const point10 = document.querySelector(".capteur-10");
             const point10Position = point10.getBoundingClientRect();
-            if (data.x > (point10Position.left-10) && data.x < (point10Position.right +210) && data.y > (point10Position.top -30) && data.y < (point10Position.bottom + 190)) {
+            if (d.x > (point10Position.left-10) && d.x < (point10Position.right +210) && d.y > (point10Position.top -30) && d.y < (point10Position.bottom + 190)) {
               setPoint10State("highlighted");
             } else {
               setPoint10State("default");
             }
-        
 
             
         })
-    );
-}, []);
+        .catch(error => console.error("Error:", error));
+      }, 1000);
+
+      return () => clearInterval(intervalId);
+    }, []);
+
+
     return(
     <div  className="map" style={{backgroundSize:"contain",backgroundRepeat:"no-repeat",backgroundPosition:"center",objectFit:"cover"}}>
       <img className="imageback" src={Ai}></img>
@@ -137,8 +135,11 @@ useEffect(() => {
       <div className={`capteur-8 ${point8State}`}></div>
       <div className={`capteur-9 ${point9State}`}></div>
       <div className={`capteur-10 ${point10State}`}></div>
+      {data.map((d, i) => (
+          
+              <Icon  key={i} style={{ left: d.x, top: d.y }} icon="material-symbols:location-on" className="location-icon"  />
 
-      <Icon  style={{ position: 'absolute', left:data.x, top: data.y}} icon="material-symbols:location-on" className="location-icon"  />
+      ))}
 
       </div>
       
